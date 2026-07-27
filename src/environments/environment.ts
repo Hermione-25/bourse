cc5df4e720316e3ca1b69505a90253f849747a64
Hermione-25 <hermioneicoutchika@gmail.com>
2026-07-27 19:18:43 +0100

    environnement

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://next-cnb4.onrender.com/api',
+  tokenStorageKey: 'bp_access_token',
+  refreshTokenStorageKey: 'bp_refresh_token',
+  googleClientId:'1080337599273-a7vv9ddqo537lp8cd6agbkcgjv03ojm2.apps.googleusercontent.com'
+};
